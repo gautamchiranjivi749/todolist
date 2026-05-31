a1e0493baaf4b2bef751a7ec4a364c0792bb298c
gautamchiranjivi749 <gautamchiranjivi749@gmail.com>
2026-05-31 16:55:17 +0545

    second commit

--- resources/views/Tasks/add.blade.php
+++ resources/views/Tasks/add.blade.php
@@ -10,12 +10,21 @@
                     @csrf
                     <div class="mb-3">
                         <input type="text" name="title" class="form-control" >
+                         @error("title")
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
                     </div>
                         <div class="mb-3">
                             <input type="datetime-local" name="deadline">
+                             @error("deadline")
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
                         </div>
                         <div class="mt-3">
                         <textarea class="form-control" name="description" rows="3"></textarea>
+                         @error("description")
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
                         {{-- <div class="mb-3">
                         <input type="checkbox" name="active" class="form-check-input " id="checkImportant">Active
                         <input type="checkbox" name="inactive" class="form-check-input " id="checkImportant">Inactive
